afa0f06a87174f9986db0baf4143792ed24b02d7
unknown <leonardohchoi@gmail.com>
2023-05-01 23:11:11 -0300

    add comments to code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import BookCreate from "./component/BookCreate";
+// The lowest common parent is the best place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
 
 function App() {
-  return <div></div>;
+  const [books, setBooks] = useState([]);
+
+  function createBook(title) {
+    console.log("add", title);
+  }
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/component/BookCreate.js
+++ src/component/BookCreate.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BookCreate() {
-  return <div></div>;
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  //handles onSubmit. When the form is submitted this function prevents default for behaviour and then calls the prop onCreate which is the createBook function.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" onChange={handleChange} value={title} />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
