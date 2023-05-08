c533b9f89f7513800865517a002962abffbf011f
unknown <leonardohchoi@gmail.com>
2023-05-08 20:38:00 -0300

    add comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+//different way to write the index
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
