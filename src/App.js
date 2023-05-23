c093fd43924341adbf26f6190e71f526267f2a72
unknown <leonardohchoi@gmail.com>
2023-05-23 14:06:55 -0300

    fix create books function with http request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
 // The lowest common parent is the best place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
@@ -28,7 +29,9 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  function createBook(title) {
+  async function createBook(title) {
+    const response = await axios.post("http://localhost:3001/books/", title);
+
     // Bad code this doesnt make react re render the component
     // books.push({ id: 123, title: title });
     // console.log(books);
@@ -36,12 +39,14 @@ function App() {
     // when inserting elements you can use slice. array.slice(1) would include everything starting at index 1 until the end and including the end of the array
     const updatedBooks = [
       ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        // this doesnt garantee that the id will never be the same but for this small app it is okay to make a random id like this
-        title,
-        // title: title (this is the same as just writing title as the key and value pairs are identical)
-      },
+      response.data,
+
+      // {
+      //   id: Math.round(Math.random() * 9999),
+      //   // this doesnt garantee that the id will never be the same but for this small app it is okay to make a random id like this
+      //   title,
+      //   // title: title (this is the same as just writing title as the key and value pairs are identical)
+      // },
     ];
     setBooks(updatedBooks);
   }
