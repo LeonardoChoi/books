3d33c100babca18fd5b33855fac1e072d8bb81a6
Leo Choi <leonardohchoi@gmail.com>
2023-05-23 13:52:23 -0300

    json server function fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import BookCreate from "./component/BookCreate";
+import axios from "axios";
 import BookList from "./component/BookList";
 // The lowest common parent is the best place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
 
@@ -32,16 +33,11 @@ function App() {
     // // console.log(books);
     // // setBooks(books);
     // // when inserting elements you can use slice. array.slice(1) would include everything starting at index 1 until the end and including the end of the array
-    // const updatedBooks = [
-    //   ...books,
-    //   {
-    //     id: Math.round(Math.random() * 9999),
-    //     // this doesnt garantee that the id will never be the same but for this small app it is okay to make a random id like this
-    //     title,
-    //     // title: title (this is the same as just writing title as the key and value pairs are identical)
-    //   },
-    // ];
-    // setBooks(updatedBooks);
+    const updatedBooks = [
+      ...books,
+     response.data
+    ];
+    setBooks(updatedBooks);
   }
 
   return (
