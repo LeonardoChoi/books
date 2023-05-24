ecd5d8127f6b0e96823b84c8db86df50dd0f0302
unknown <leonardohchoi@gmail.com>
2023-05-24 20:38:09 -0300

    working on fixing editing book title function

--- src/App.js
+++ src/App.js
@@ -16,20 +16,24 @@ function App() {
   }, []);
 
   const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost3001/books/${id}`, {
+    const response = await axios.put(`http://localhost:3001/books/` + id, {
       title: newTitle,
     });
 
+    console.log(response);
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, ...response.data };
+        return { ...book, title: newTitle };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -37,7 +41,7 @@ function App() {
   };
 
   async function createBook(title) {
-    const response = await axios.post("http://localhost:3001/books/", title);
+    const response = await axios.post("http://localhost:3001/books", { title });
 
     // Bad code this doesnt make react re render the component
     // books.push({ id: 123, title: title });
