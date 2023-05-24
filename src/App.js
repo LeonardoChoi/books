541c37522a0bcfbad876c4584ab7a6d2fa93f1f4
unknown <leonardohchoi@gmail.com>
2023-05-24 17:24:54 -0300

    made editBookById function make a http request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
-// The lowest common parent is the best place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
+// The lowest common parent is the best place to place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -11,8 +11,15 @@ function App() {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
   };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
         return { ...book, title: newTitle };
