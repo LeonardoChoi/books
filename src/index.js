760ab3536620139b1e9e67cd953a001b4d4fea71
unknown <leonardohchoi@gmail.com>
2023-05-27 11:39:32 -0300

    make context provider for valueToShare

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-
+import BooksContext from "./context/books.js";
 //different way to write the index
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <div>
+    <BooksContext.Provider value={5}>
+      <App />
+    </BooksContext.Provider>
+  </div>
+);
