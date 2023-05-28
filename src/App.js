67091b2db8cce493cfe79a4c14128be6c4c7a7c1
unknown <leonardohchoi@gmail.com>
2023-05-28 14:06:58 -0300

    move fucntions to /context/books.js and got fetchBooks function from there to the App component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useContext } from "react";
+
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
+import BooksContext from "./context/books";
 // The lowest common parent is the best place to place the state of your app. The lowest common parent is the parent that has all the components that will use the state under it (it's children) so in this app we would place the state here in the App.js file because all its children need to access the state.
 
 function App() {
+  const { fetchBooks } = useContext(BooksContext);
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    console.log(response);
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, title: newTitle };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", { title });
-
-    // Bad code this doesnt make react re render the component
-    // books.push({ id: 123, title: title });
-    // console.log(books);
-    // setBooks(books);
-    // when inserting elements you can use slice. array.slice(1) would include everything starting at index 1 until the end and including the end of the array
-    const updatedBooks = [
-      ...books,
-      response.data,
-
-      // {
-      //   id: Math.round(Math.random() * 9999),
-      //   // this doesnt garantee that the id will never be the same but for this small app it is okay to make a random id like this
-      //   title,
-      //   // title: title (this is the same as just writing title as the key and value pairs are identical)
-      // },
-    ];
-    setBooks(updatedBooks);
-  };
-
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import axios from "axios";
 
 const BooksContext = createContext();
 
@@ -8,6 +9,53 @@ function Provider({ children }) {
   const fetchBooks = async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
+  };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    console.log(response);
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", { title });
+
+    // Bad code this doesnt make react re render the component
+    // books.push({ id: 123, title: title });
+    // console.log(books);
+    // setBooks(books);
+    // when inserting elements you can use slice. array.slice(1) would include everything starting at index 1 until the end and including the end of the array
+    const updatedBooks = [
+      ...books,
+      response.data,
+
+      // {
+      //   id: Math.round(Math.random() * 9999),
+      //   // this doesnt garantee that the id will never be the same but for this small app it is okay to make a random id like this
+      //   title,
+      //   // title: title (this is the same as just writing title as the key and value pairs are identical)
+      // },
+    ];
+    setBooks(updatedBooks);
   };
 
   return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>;
